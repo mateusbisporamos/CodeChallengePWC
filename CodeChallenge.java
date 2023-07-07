32c80c21d23b953679e684ce4118bc8ec7680ae7
mateusbisporamos <mateusbisporamos.dev@gmail.com>
2023-07-06 22:40:02 -0300

    duplicates removal

--- CodeChallenge.java
+++ CodeChallenge.java
@@ -1,10 +1,10 @@
 public class CodeChallenge
 {
 	public static void main(String[] args) {
-	    String str = "Hello, World! OpenAI is amazing.";
+	    String str = "Hello, World!";
 	    
 	   inversion(str);
-	   
+	   duplicates(str);
 	}
 	
 	public static void inversion(String str) {
@@ -13,6 +13,19 @@ public class CodeChallenge
 	    for(int counter = words.length-1; counter >= 0; counter--){
 	        strInversion += words[counter] + " ";
 	    }
+
 	    System.out.println(strInversion.trim());
 	}
+
+    public static void duplicates(String str) {
+        char[] letters = str.toCharArray();
+        String strDuplicates = "";
+        for (char c : letters) {
+            if(!(strDuplicates.contains(Character.toString(c)))){
+                strDuplicates += c;
+            }
+        }
+
+        System.out.println(strDuplicates.trim());
+    }
 }
